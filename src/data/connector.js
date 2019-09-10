7ab2a93a7ac33403898395a2420eb163a15671c8
richard <inf.darzegmail.com>
2019-09-10 02:48:00 -0500

    adding mongoose connector

---
+++ src/data/connector.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+dotenv.config();
+
+const uri = `${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
+const options = {
+  auth: {
+    user: process.env.MONGODB_USER,
+    password: process.env.MONGODB_PASS,
+  },
+  authSource: 'admin',
+  useNewUrlParser: true,
+  useCreateIndex: true,
+};
+
+const initialize = () => {
+  mongoose.Promise = global.Promise;
+
+  mongoose.connect(uri, options);
+
+  mongoose.connection.on('connected', () => console.log("we're connected!"));
+
+  mongoose.connection.on('error', err =>
+    console.log(`Mongoose connection error: ${err}`)
+  );
+
+  mongoose.connection.on('disconnected', () =>
+    console.log('Mongoose connection disconnected!')
+  );
+
+  mongoose.plugin(uniqueValidator);
+
+  return true;
+};
+
+export { initialize };
