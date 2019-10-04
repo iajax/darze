4ad4df79d39c6327c9c6227fb199317b57a47ff7
richard <inf.darzegmail.com>
2019-10-04 03:31:23 -0500

    user mutation

--- services/user/src/resolvers.js
+++ services/user/src/resolvers.js
@@ -4,6 +4,21 @@ const resolvers = {
     user: (obj, args, ctx) => ctx.models.User.findOne({ _id: args.id }),
   },
 
+  Mutation: {
+    createUser: (_, args, ctx) => ctx.models.User.create(args.input),
+    updateUser: (_, args, ctx) =>
+      ctx.models.User.findOneAndUpdate(ctx.user._id, args.input, {
+        new: true,
+      })
+        .select('-password')
+        .lean()
+        .exec(),
+    removeProduct: (_, args, ctx) =>
+      ctx.models.User.findByIdAndRemove(args._id)
+        .lean()
+        .exec(),
+  },
+
   User: {
     __resolveReference: (user, ctx) =>
       ctx.models.User.findOne({ _id: user.id }),
--- services/user/src/typeDefs.js
+++ services/user/src/typeDefs.js
@@ -9,12 +9,30 @@ const schema = gql`
     # The users' username
     username: String
   }
+  input UserInput {
+    first_name: String
+    username: String
+    email: String
+    password: String
+  }
+  input UpdateUserInput {
+    first_name: String
+    username: String
+    email: String
+    password: String
+  }
   # Queries from user service
   extend type Query {
     # List of all our users
     allUsers: [User]
     # A single user
     user(id: ID!): User
+  }
+
+  extend type Mutation {
+    createUser(input: UserInput!): User!
+    updateUser(id: ID!, input: UpdateUserInput!): User!
+    removeProduct(id: ID!): User!
   }
 `;
 
